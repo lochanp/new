7708922805e656bcbaefc6e9320febcabd72672a
lochan07 <lochan@binaryveda.com>
2022-11-28 19:45:56 +0530

    removed unused vars

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
 import React, {useRef} from 'react'
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
 import About from '../About';
 import Work from '../Work';
 import Contact from '../Contact';
