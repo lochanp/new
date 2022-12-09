bf1397f2dd6a5be1df26d68e3765610ad836f138
lochan07 <lochan@binaryveda.com>
2022-12-09 23:03:52 +0530

    contact page added

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import Hey1 from '../../assets/images/hey.png'
+import Hey2 from '../../assets/images/hey1.png'
 
 const Contact = () => {
   return (
-    <div>
-      <p>I  nterested in working with me?</p>
-      <p>Get In Touch. I am always up for a discussion</p>
-      <button>Send a Mail</button>
+    <div className='contact-page'>
+      <p className='heading'>Interested in working with me?</p>
+      <div className='main'>
+        <div className='info-container'>
+          <p className='info'>Get In Touch.<br/> I am always up for a discussion</p>
+          <div className='mail'><a href='mailto:lochan.potdar1998@gmail.com'>Send a Mail</a></div>
+        </div>
+        <div className='img-container'>
+        <img className='hey-1' src={Hey1} alt=''></img>
+          <img className='hey-2' src={Hey2} alt=''></img>
+          
+        </div>
+      </div>
     </div>
   )
 }
