60758f9c083cefde37e3dd074fe186fc51a02b4c
lochan07 <lochan@binaryveda.com>
2023-03-20 18:11:27 +0530

    build fail fixes

--- src/pages/Work/index.js
+++ src/pages/Work/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { workData } from '../../data';
-import { Link } from "react-router-dom";
 
 const Work = () => {
   // const navigate = useNavigate();
@@ -9,7 +8,7 @@ const Work = () => {
       <p className='heading'>Here are some of my projects..</p>
       <div className='grid-container'>
       {workData.map((e,i) => (
-          <a className='box' key={i} href={e.link} target='_blank'>
+          <a className='box' rel="noreferrer" key={i} href={e.link} target='_blank'>
             <div className='img-box'><img src={e.imgUrl} alt='project pic'></img></div>
             <p className='title'>{e.title}</p>
           </a>
