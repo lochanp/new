ab45087376fe244a5bb927711f757ac88ed83ddc
lochan07 <lochan@binaryveda.com>
2024-02-07 12:19:17 +0530

    added the sun icon

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Sun from '../assets/images/icons8-sun.svg';
 const DarkModeToggle = ({ handleChange, isChecked }) => {
   return (
     <div>
@@ -8,11 +8,7 @@ const DarkModeToggle = ({ handleChange, isChecked }) => {
         onChange={handleChange}
         onClick={handleChange}
         id='theme-icon'
-        src={
-          isChecked
-            ? 'https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg'
-            : 'https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg'
-        }
+        src={isChecked ? 'https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg' : Sun}
         alt='ERR'
       />
     </div>
